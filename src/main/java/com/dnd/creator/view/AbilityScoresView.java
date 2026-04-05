46347df71618eb136b81b4c6042b743d5e80cd37
markic <m.trkulja@students.htl-leonding.ac.at>
2026-04-05 16:24:51 +0200

    added the missing imports and fields

---
+++ src/main/java/com/dnd/creator/view/AbilityScoresView.java
@@ -0,0 +1,24 @@
+package com.dnd.creator.view;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AbilityScoresView {
+    private Parent root;
+
+    @FXML
+    private HBox valuePool;
+
+    @FXML
+    private Label strValue;
